cd1a5a26a3432bd86d4f430618b3ba549feff464
Jaskaran Singh <jassi.dev@icloud.com>
2026-09-25 03:05:51 +0530

    now you can send and receive data
    yes!!!! with the help of (mr shadowbanned on github on TPH in discord) now you can send data to a socket using smth like netcat and then the socket will print your data to the server which you sent as a client

--- main.c
+++ main.c
@@ -10,24 +10,26 @@ int main(void) {
   struct sockaddr_in addr;
   int abc = socket(AF_INET, SOCK_STREAM, 0);
   addr.sin_family = AF_INET;
+  char buffer[1000];
   addr.sin_port = htons(6969);
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(&(addr.sin_zero), '\0', 8);
-  int random_int = bind(abc, (struct sockaddr *)&addr, sizeof(addr));
+  memset(buffer, '\0', 1000);
+  char random_int = bind(abc, (struct sockaddr *)&addr, sizeof(addr));
   listen(abc, 10);
   int i = 1;
-  const char *text_to_be_sent = "hey bitch \n";
+  const char *text_to_be_sent = "your data has been sent bitch \n";
   socklen_t addr_len = sizeof(struct sockaddr);
   while (i == 1) {
     int abcd = accept(abc, (struct sockaddr *)&addr, &addr_len);
     if (abcd == -1) {
       perror("some oopsie happend");
       return 1;
-    } else {
-      while (i == 1) {
-        send(abcd, text_to_be_sent, strlen(text_to_be_sent) + 1, 0);
-        sleep(1);
+    } 
+    else {
+      ssize_t data_collected = recv(abcd, buffer, sizeof(buffer), 0);
+      printf("the data you sent is: %s\n", buffer);
+      send(abcd, text_to_be_sent, strlen(text_to_be_sent) + 1, 0);
       }
     }
   }
-}
